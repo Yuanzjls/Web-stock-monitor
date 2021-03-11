936471ff229d9844f35d563a8c334b0da2b70148
Yuan Zhou <yzhou@windsormachine.com>
2021-03-11 15:23:40 -0500

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Todo from "./components/Todo";
 import Form from "./components/Form";
 import React, {useState, useRef, useEffect} from "react";
 import {nanoid} from "nanoid";
+// import useInterval from 'react-useinterval';
+
 const axios = require('axios');
 const moment = require("moment-timezone");
 const apiKey = "c1433cn48v6s4a2e39q0";
@@ -12,7 +14,8 @@ const preUrl = "https://finnhub.io/api/v1/quote?symbol=";
 function App(props) {
   const [stocks, setStocks] = useState(props.stocks);
   const prevTaskLenght = usePrevious(stocks.length);
-  const [timeUpdate, SetTime] = useState("00:00:00 AM");
+  const [timeUpdate, SetTime] = useState(moment().format('LT'));
+  const [active, SetActive] = useState(false);
   function usePrevious(value) {
     const ref = useRef();
     useEffect(()=>{
@@ -25,30 +28,52 @@ function App(props) {
   priceInfo={stock.priceInfo}/>));
   const tasksNoun = stockList.length!==1? 'stocks' : 'stock';
   const headingText = `${stockList.length} ${tasksNoun}`;
-  const apiList = stocks.map(stock=>({apiUrl:`${preUrl}${stock.name}&token=${apiKey}`}));
+  
 
   useEffect(()=>{
     if (stockList.length-prevTaskLenght ===-1){
       listHeadingRef.current.focus();
     }
-  }, [stocks.length, prevTaskLenght]);
+  }, [stockList.length, prevTaskLenght]);
 
+  useInterval(()=>{fetchData();}, 10000)
 
-  useEffect(()=>{
-    
-    const promises = apiList.map(api=>axios(api.apiUrl));
-    // console.log(promises);
+  function fetchData() {
+    let apiList = stocks.map(stock=>({apiUrl:`${preUrl}${stock.name}&token=${apiKey}`}));
+    let promises = apiList.map(api=>axios(api.apiUrl));
+    console.log(stocks);
+
     axios.all(promises).then(axios.spread((...responses)=>{
       const newStocks = stocks.map((stock, index)=>{
         return {...stock, priceInfo:{'pricePre':responses[index].data.pc, 'priceCur':responses[index].data.c}};
       });
       setStocks(newStocks);
-      console.log(newStocks);
+      SetTime(moment().format('LT'));
+      
     })).catch(errors => {
       console.log(errors);
     });
+  } 
+
+  function useInterval(callback, delay) {
+    const savedCallback = useRef();
   
-  }, [...apiList.map(api=>api.apiUrl)]);
+    useEffect(() => {
+      savedCallback.current = callback;
+    });
+  
+    useEffect(() => {
+      function tick() {
+        savedCallback.current();
+      }
+      fetchData();
+
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }, [delay]);
+  }
+  
+
 
 
 
@@ -56,6 +81,8 @@ function App(props) {
   function deleteTask(id){
     const remainingTasks = stocks.filter(task=> id !== task.id);
     setStocks(remainingTasks);
+    console.log(remainingTasks);
+    // SetActive(!active);
   }
 
   function editTask(id, newName){
@@ -66,11 +93,13 @@ function App(props) {
       return task;
     });
     setStocks(editedTasks);
+    // SetActive(!active);
   }
 
   function addTask(name){
-    const newTask = {id:"todo-" + nanoid(), name:name};
+    const newTask = {id:"todo-" + nanoid(), name:name, priceInfo:{'pricePre':0, 'priceCur':0}};
     setStocks([...stocks, newTask]);
+    // SetActive(active);
   }
  
   
@@ -78,6 +107,7 @@ function App(props) {
   return (
     <div className="todoapp stack-large">
       <h1>Stock Prices</h1>
+        <p style={{textAlign:"center"}}>Update at {timeUpdate}</p>
         <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
       
